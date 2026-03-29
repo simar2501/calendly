05e990df9aaa8f11b45e8871122ee90753c3bc33
simar2501 <23BAI70318@CUCHD.IN>
2026-03-30 01:19:20 +0530

    fix eslint errors

--- frontend/src/pages/BookingPage.jsx
+++ frontend/src/pages/BookingPage.jsx
@@ -16,6 +16,7 @@ const BookingPage = ({ singleUse = false }) => {
   const [currentMonth, setCurrentMonth] = useState(dayjs());
   const [loading, setLoading] = useState(false);
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => { fetchEvent(); }, [slug, token]);
 
   const fetchEvent = async () => {

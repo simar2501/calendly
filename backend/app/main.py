1c44e0c99f1101fafa28b01b28e4fec1933044ac
simar2501 <23BAI70318@CUCHD.IN>
2026-03-30 01:33:31 +0530

    add vercel url to cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://calendly-clone-goa5.vercel.app",
+        "https://calendly-kappa.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],

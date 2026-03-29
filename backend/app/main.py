71037b87826e052455c452ea91b256f61e87562f
simar2501 <23BAI70318@CUCHD.IN>
2026-03-30 01:13:30 +0530

    update cors and axios

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,11 @@ def create_default_user():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://calendly-clone-goa5.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
